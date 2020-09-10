a2f17d2e7654597684850547b19edba1e51c79eb
rigo5632 <rmquiroz5@gmail.com>
2020-09-10 13:45:04 -0600

    Added Redirect Output to shell

--- shell2.py
+++ shell2.py
@@ -5,7 +5,8 @@ import os, sys, re
 stdInput = 0
 stdDisplay = 1
 stdErrorDisplay = 2
-while 1:
+
+while True:
     rc = os.fork()
     if rc < 0:
         os.write(stdErrorDisplay, ("fork failed, returning %d\n" % rc).encode())
@@ -13,23 +14,42 @@ while 1:
 
     elif rc == 0:                   # child
         tokens = []
+        redirectOutput = False
+        redirectInput = False
         
         os.write(stdDisplay, '$ '.encode())
         userInput = os.read(0, 100)
         
         line = re.split(b'\s', userInput)
         
-        for token in line: tokens.append(token.decode()) if token != b'' else None
+        for token in line:
+            if token == b'>':
+                redirectOutput = True
+                token = b''
+            elif token == b'<':
+                redirectInput = True
+                token = b''
+            tokens.append(token.decode()) if token != b'' else None
+
         if len(tokens) == 1 and tokens[0] == 'exit': sys.exit(2)
         
+        if redirectOutput:
+            os.close(1)
+            os.open(tokens[len(tokens)-1], os.O_CREAT | os.O_WRONLY)
+            os.set_inheritable(1, True)
+            tokens = tokens[:len(tokens)-1]
+            redirect = False
+        elif redirectInput:
+            print('Do Something Cool')
+            redirectInput = True
+
         if tokens:
             for dir in re.split(":", os.environ['PATH']):
                 program = "%s/%s" % (dir, tokens[0])
                 try:
                     os.execve(program, tokens, os.environ)
-                except FileNotFoundError:            
-                    pass                              
-
+                except FileNotFoundError:
+                    pass
             os.write(2, ("Child:    Could not exec %s\n" % tokens[0]).encode())
             sys.exit(1)
         sys.exit(0)
