f334a8a45d79cb1436e360d99faf20fdeb827c34
rigo5632 <rmquiroz5@gmail.com>
2020-09-14 22:41:53 -0600

    Fixed Parent Shell

--- shell2.py
+++ shell2.py
@@ -2,61 +2,85 @@
 
 import os, sys, re
 
-stdInput = 0
-stdDisplay = 1
-stdErrorDisplay = 2
+redirect ={'inTokens': False, 'fileDescriptor': None, 'file': None, 'previousFD': None}
 
+def getTokens(userInput):
+    line = re.split(b'\s', userInput)
+    fullPath = re.search('/', line[0].decode())
+    tokens = []
+    
+    for token in line:
+        if token == b'>':
+            redirect['inTokens'] = True
+            redirect['fileDescriptor'] = 1
+            redirect['file'] = os.O_CREAT | os.O_WRONLY
+            redirect['previousFD'] = os.dup(1)
+            token = b''
+        elif token == b'<':
+            redirect['inTokens'] = True
+            redirect['fileDescriptor'] = 0
+            redirect['file'] = os.O_RDONLY
+            redirect['previousFD'] = os.dup(0)
+            token = b''
+        tokens.append(token.decode()) if token != b'' else None
+
+    if len(tokens) == 1 and tokens[0] == 'exit': sys.exit(0)
+    if len(tokens) == 2 and tokens[0] == 'cd':
+        os.chdir(tokens[1])
+        tokens = []
+        fullPath = None
+    
+    return tokens, fullPath
+        
+
+def shell():
+    #prompt = os.environ['PS1'] if os.environ['PS1'] != '' else '$ '
+    prompt = '$ '
+    
+    os.write(1, prompt.encode())
+    userInput = os.read(0, 1024)
+
+    return getTokens(userInput)
+
+tokens, fullPath = shell()
 while True:
     rc = os.fork()
     if rc < 0:
         os.write(stdErrorDisplay, ("fork failed, returning %d\n" % rc).encode())
         sys.exit(1)
-
-    elif rc == 0:                   # child
-        tokens = []
-        redirect = {'inTokens': False, 'fileDescriptor': None, 'file': None}
-        
-        prompt = os.environ['PS1'] if os.environ['PS1'] != '' else '$ '
-        os.write(stdDisplay, prompt.encode())
-        userInput = os.read(0, 100)
-        
-        line = re.split(b'\s', userInput)
-        print(line)
-        
-        for token in line:
-            if token == b'>':
-                redirect['inTokens'] = True
-                redirect['fileDescriptor'] = 1
-                redirect['file'] = os.O_CREAT | os.O_WRONLY
-                token = b''
-            elif token == b'<':
-                redirect['inTokens'] = True
-                redirect['fileDescriptor'] = 0
-                redirect['file'] = os.O_RDONLY
-                token = b''
-            tokens.append(token.decode()) if token != b'' else None
-
-        if len(tokens) == 1 and tokens[0] == 'exit': sys.exit(2)
-        
+    elif rc == 0:# child
         if redirect['inTokens']:
             os.close(redirect['fileDescriptor'])
             os.open(tokens[len(tokens)-1], redirect['file'])
             os.set_inheritable(redirect['fileDescriptor'], True)
             tokens = tokens[:len(tokens)-1]
+            
+        if tokens:
+            if not fullPath:
+                for dir in re.split(":", os.environ['PATH']):
+                    program = "%s/%s" % (dir, tokens[0])
+                    try:
+                        os.execve(program, tokens, os.environ)
+                    except FileNotFoundError:
+                        pass
+                os.write(2, ("%s Cannot be found\n" % tokens[0]).encode())
+                sys.exit(0)
+            else:
+                try:
+                    os.execve(tokens[0], tokens, os.environ)
+                except FileNotFoundError:
+                    pass
+                os.write(2, ("%s Cannot be found\n" % tokens[0]).encode())
+                sys.exit(0)
+        sys.exit(0)
+    else:
+        exitCode = os.wait()
+        if redirect['inTokens']:
+            os.dup2(redirect['previousFD'], redirect['fileDescriptor'])
+            os.close(redirect['previousFD'])
+            redirect['previousFD'] = None
             redirect['inTokens'] = False
             redirect['fileDescriptor'] = None
             redirect['file'] = None
-            
-        if tokens:
-            for dir in re.split(":", os.environ['PATH']):
-                program = "%s/%s" % (dir, tokens[0])
-                try:
-                    os.execve(program, tokens, os.environ)
-                except FileNotFoundError:
-                    pass
-            os.write(2, ("Child:    Could not exec %s\n" % tokens[0]).encode())
-            sys.exit(1)
-        sys.exit(0)
-    else:
-        childPidCode = os.wait()
-        if childPidCode[1] == 512: sys.exit(1)
+        if exitCode[1] != 0: os.write(1, ('Program Terminated with exit code: %d\n' % exitCode[1]).encode())
+        tokens, fullPath = shell()
